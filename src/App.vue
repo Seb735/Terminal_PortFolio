5ba1111cdea003ce39a552b3265ba2bb585c09cd
seb <duclutsebastien@yahoo.com>
2022-01-09 20:47:28 +0100

    first Commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="page">
+
+    <div class="terminal">
+      <div class="cmd">
+        <q-input
+          type="text"
+          prefix="$>>"
+          autofocus
+          dense
+          borderless
+          v-model="text"
+        />
+      </div>
+    </div>
+
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70%;
+  width: 90%;
+
+  .terminal {
+    background-color: #F2C037;
+    height: 500px;
+    width: 700px;
+  }
+}
+</style>
